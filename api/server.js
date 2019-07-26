d089ed932a7c5524c5130cd944d36ec007633a72
Awa Melvine <melvineawa@gmail.com>
2019-07-26 20:16:19 +0100

    chore(eslint): set up eslint

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(helmet())
 
 app.use('*', (req, res) => {
-    res.status(404).json({ error: 'Page Not Found' });
+    res.status(404).json({ error: 'Page Not Found' }); 
 });
 
-module.exports = app;
+module.exports = app; 
