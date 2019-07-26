4fcb54e4509913590e506c340d4e00a29818618d
Awa Melvine <melvineawa@gmail.com>
2019-07-26 20:53:37 +0100

    chore(cors): add cors middleware

--- api/server.js
+++ api/server.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import helmet from 'helmet';
+import cors from 'cors';
 
 const app = express();
 
-app.use(helmet())
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
 app.use('*', (req, res) => {
-    res.status(404).json({ error: 'Page Not Found' }); 
+    res.status(404).json({ error: 'Page Not Found' });
 });
 
 export default app;
