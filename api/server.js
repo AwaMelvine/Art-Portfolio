d209cca0afcc14ed926f99fd117b2c0075c35fa5
Awa Melvine <melvineawa@gmail.com>
2019-07-29 13:58:57 +0100

    chore(server): configure server routing to use client

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
+import path from 'path';
 import trimmer from 'express-trimmer';
 import appRoutes from './routes';
 
@@ -13,14 +14,13 @@ app.use(trimmer);
 
 app.use(appRoutes);
 
+app.use(express.static(path.join(__dirname, "..", "client", "build")))
 
-app.use('*', (req, res) => {
-    res.status(404).json({ error: 'Page Not Found' });
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 });
 
-
 const { PORT = 8000, DB_ENV } = process.env;
-
 if (DB_ENV !== 'testing') {
     app.listen(PORT, console.log(`***  Server running on localhost:${PORT}  ***`));
 }
