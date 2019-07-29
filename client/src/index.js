9037dd6800536b36066b785d8b9d109624948a17
Awa Melvine <melvineawa@gmail.com>
2019-07-29 21:17:11 +0100

    chore(redux) create redux store

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import "./index.css";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './store';
+
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>, document.getElementById('root'));
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
+    , document.getElementById('root'));
