eaa7cb023e8c1a5e7c8612d697856b4b39857be0
Awa Melvine <melvineawa@gmail.com>
2019-07-29 07:56:27 +0100

    feat(validation) validate unique post title

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -25,6 +25,9 @@ export const addPost = async (req, res) => {
         const post = await Post.insert(req.body);
         res.status(201).json({ data: post });
     } catch (error) {
+        if (error.code === '23505' && error.constraint === 'posts_title_unique') {
+            return res.status(400).json({ errors: { title: 'Post title already exists' } })
+        }
         res.status(500).json({ error: 'Internal Server Error' });
     }
 };
@@ -35,6 +38,9 @@ export const updatePost = async (req, res) => {
         const post = await Post.update(id, req.body);
         res.status(200).json({ data: post });
     } catch (error) {
+        if (error.code === '23505' && error.constraint === 'posts_title_unique') {
+            return res.status(400).json({ errors: { title: 'Post title already exists' } })
+        }
         res.status(500).json({ error: 'Internal Server Error' });
     }
 };
