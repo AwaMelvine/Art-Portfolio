65ec8ed303f4db0d93ef104403ee0eeabf7022da
Awa Melvine <melvineawa@gmail.com>
2019-07-30 16:10:48 +0100

    chore(database): create migration for likes table

---
+++ api/db/migrations/20190730160617_003_createLikesTable.js
@@ -0,0 +1,24 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable('likes', table => {
+        table.increments();
+        table.integer('post_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('posts')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE');
+        table.integer('user_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE');
+    });
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('likes');
+};
