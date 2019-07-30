804f3a7c0a5976b2e43d590e1664d65a7efc6219
Awa Melvine <melvineawa@gmail.com>
2019-07-30 21:33:13 +0100

    feat(auth): apply auth and authorization middleware

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -8,13 +8,14 @@ import {
     likePost
 } from '../controllers/posts';
 import { validateCreate, validateUpdate, validatePostId } from '../helpers/validatePost';
+import { authenticate, artistsOnly } from '../helpers/authenticate';
 const router = new Router();
 
 router.get('/', getPosts);
 router.get('/:id', validatePostId, getPostById);
-router.post('/', validateCreate, addPost);
-router.post('/likes/:id', likePost);
-router.put('/:id', validatePostId, validateUpdate, updatePost);
-router.delete('/:id', validatePostId, deletePost);
+router.post('/', authenticate, artistsOnly, validateCreate, addPost);
+router.post('/likes/:id', authenticate, validatePostId, likePost);
+router.put('/:id', authenticate, artistsOnly, validatePostId, validateUpdate, updatePost);
+router.delete('/:id', artistsOnly, authenticate, validatePostId, deletePost);
 
 export default router;
