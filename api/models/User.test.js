f09d56c05fe92c1c01b2be880f204948f7a7fc25
Awa Melvine <melvineawa@gmail.com>
2019-07-31 11:58:55 +0100

    chore(tests) fix failing tests

--- api/models/User.test.js
+++ api/models/User.test.js
@@ -2,62 +2,58 @@ import "@babel/polyfill";
 import User from '../models/User';
 import db from '../db/dbConfig';
 
-const admin = {
-    role: 'user',
-    username: 'John',
-    email: 'john@john.com',
-    password: 'mel'
-};
-
-const artist = {
-    role: 'artist',
-    username: 'Awa',
-    email: 'awa@awa.com',
-    password: 'mel'
-};
 
 const user = {
+    id: 1,
     role: 'admin',
     username: 'Melvine',
     email: 'melvine@melvine.com',
     password: 'mel'
 };
 
+const artist = {
+    id: 2,
+    role: 'artist',
+    username: 'Awa',
+    email: 'awa@awa.com',
+    password: 'mel'
+};
+
+
 const changes = {
-    username: 'Melvine Awa',
+    username: 'Awa Melvine',
     email: 'melvine@awa.com',
 };
 
 
 
 beforeAll(async () => {
-    await db('users').truncate();
-    await User.insert(admin);
-    await User.insert(artist);
+    await db.raw('TRUNCATE TABLE users CASCADE');
 });
 
 afterAll(async () => {
-    await db('users').truncate();
+    await db.raw('TRUNCATE TABLE users CASCADE');
 });
 
 describe('User Model', () => {
     it('gets an array of all users', async () => {
+        await User.insert(user);
+        const users = await User.get();
+        expect(users.length).toBe(1);
+    });
+    it('gets a user by Id', async () => {
+        const createdUser = await User.get(1);
+        expect(createdUser.email).toBe(user.email);
+    });
+    it('creates a new user', async () => {
+        await User.insert(artist);
         const users = await User.get();
         expect(users.length).toBe(2);
     });
-    it('gets a user by Id', async () => {
-        const user = await User.get(1);
-        expect(user.email).toBe('john@john.com');
-    });
-    it('creates a new user', async () => {
-        await User.insert(user);
-        const users = await User.get();
-        expect(users.length).toBe(3);
-    });
     it('updates user info', async () => {
-        await User.update(3, changes);
-        const updatedUser = await User.get(3);
-        expect(updatedUser.username).toEqual('Melvine Awa');
+        await User.update(2, changes);
+        const updatedUser = await User.get(2);
+        expect(updatedUser.username).toEqual(changes.username);
     });
     it('deletes a user', async () => {
         await User.remove(3);
